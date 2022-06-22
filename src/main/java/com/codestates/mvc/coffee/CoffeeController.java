5e37722308b5605a64867646b3dfe9d251ca66d9
Kyutae-Chae <ktchae83@gmail.com>
2022-06-22 10:14:23 +0900

    change response from String to ResponseEntity

--- src/main/java/com/codestates/mvc/coffee/CoffeeController.java
+++ src/main/java/com/codestates/mvc/coffee/CoffeeController.java
@@ -1,38 +1,40 @@
 package com.codestates.mvc.coffee;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/coffees")
 public class CoffeeController {
 
     @PostMapping
-    public String postCoffee(@RequestParam("engName") String engName,
-                             @RequestParam("korName") String korName,
-                             @RequestParam("price") int price) {
+    public ResponseEntity postCoffee(@RequestParam("engName") String engName,
+                                     @RequestParam("korName") String korName,
+                                     @RequestParam("price") int price) {
         System.out.println("# engName = " + engName);
         System.out.println("# korName = " + korName);
         System.out.println("# price = " + price);
 
-        String response =
-                "{\"" +
-                        "engName\":\"" + engName + "\"," +
-                        "\"korName\":\"" + korName + "\",\"" +
-                        "price\":\"" + price +
-                        "\"}";
-        return response;
+        Map<String, Object> map = new HashMap<>();
+        map.put("engName", engName);
+        map.put("korName", korName);
+        map.put("price", price);
+        return new ResponseEntity(map, HttpStatus.CREATED);
     }
 
     @RequestMapping("/{coffee-id}")
-    public String getCoffee(@PathVariable("coffee-id") long coffeeId) {
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
         System.out.println("# coffeeId = " + coffeeId);
-
-        return null;
+        return new ResponseEntity(HttpStatus.OK);
     }
 
     @RequestMapping
-    public String getCoffees() {
+    public ResponseEntity getCoffees() {
         System.out.println("CoffeeController.getCoffees");
-        return null;
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/codestates/mvc/member/MemberController.java
+++ src/main/java/com/codestates/mvc/member/MemberController.java
@@ -1,37 +1,40 @@
 package com.codestates.mvc.member;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "/v1/members", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping("/v1/members")
 public class MemberController {
 
     @PostMapping
-    public String postMember(@RequestParam("email") String email,
+    public ResponseEntity postMember(@RequestParam("email") String email,
                              @RequestParam("name") String name,
                              @RequestParam("phone") String phone) {
         System.out.println("# email = " + email);
         System.out.println("# name = " + name);
         System.out.println("# phone = " + phone);
-        String response =
-                "{\"" +
-                        "email\":\"" + email + "\"," +
-                        "\"name\":\"" + name + "\",\"" +
-                        "phone\":\"" + phone +
-                        "\"}";
-        return response;
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("name", name);
+        map.put("phone", phone);
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
     @GetMapping("/{member-id}")
-    public String getMember(@PathVariable("member-id") long memberId) {
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         System.out.println("# memberId = " + memberId);
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public String getMembers() {
+    public ResponseEntity getMembers() {
         System.out.println("MemberController.getMembers");
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
