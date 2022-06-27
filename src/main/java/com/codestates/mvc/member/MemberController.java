460215dd50dd795d942ced0fd8e23429d30b4fa7
Kyutae-Chae <ktchae83@gmail.com>
2022-06-27 09:39:51 +0900

    add mapper

--- src/main/java/com/codestates/mvc/member/MemberController.java
+++ src/main/java/com/codestates/mvc/member/MemberController.java
@@ -8,43 +8,61 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/v1/members")
+@RequestMapping("/v4/members")
 @Validated
 public class MemberController {
 
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+
+    public MemberController(MemberService memberService, MemberMapper mapper) {
+        this.memberService = memberService;
+        this.mapper = mapper;
+    }
+
     @PostMapping
     public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Client-Geo-Location", "Korea,Seoul"); //test
-        return new ResponseEntity<>(memberPostDto, headers, HttpStatus.CREATED);
+        Member member = mapper.memberPostDtoToMember(memberPostDto);
+
+        Member response = memberService.createMember(member);
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{member-id}")
-    public ResponseEntity patchMember(@PathVariable("member-id") @Min(1) long memberId,
+    public ResponseEntity patchMember(@PathVariable("member-id") @Positive long memberId,
                                       @Valid @RequestBody MemberPatchDto memberPatchDto) {
         memberPatchDto.setMemberId(memberId);
-        memberPatchDto.setName("홍길동2");
 
-        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+        Member member = mapper.memberPatchDtoToMember(memberPatchDto);
+
+        Member response = memberService.updateMember(member);
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.OK);
     }
 
     @GetMapping("/{member-id}")
-    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
-        System.out.println("# memberId = " + memberId);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity getMember(@PathVariable("member-id") @Positive long memberId) {
+        Member response = memberService.findMember(memberId);
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(response), HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity getMembers(HttpServletResponse response) {
-        response.addHeader("Client-Geo-Location", "Korea,Seoul");
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity getMembers() {
+        List<Member> members = memberService.findMembers();
+        List<MemberResponseDto> response = members.stream()
+                .map(member -> mapper.memberToMemberResponseDto(member))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{member-id}")
-    public ResponseEntity deleteMember(@PathVariable("member-id") long memberId) {
+    public ResponseEntity deleteMember(@PathVariable("member-id") @Positive long memberId) {
+        System.out.println("# delete member");
+        memberService.deleteMember(memberId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
